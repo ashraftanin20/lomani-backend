7201ac03a40b3712509d190e04578c663c488da7
ashraftanin <ashraf.tanin@gmail.com>
2023-04-04 11:49:29 +0300

    Updated cors policy

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,18 @@ import uploadRouter from './routers/uploadRouter.js';
 dotenv.config();
 
 const app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://lomani.onrender.com");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, HEAD, OPTIONS, POST, PUST, DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
